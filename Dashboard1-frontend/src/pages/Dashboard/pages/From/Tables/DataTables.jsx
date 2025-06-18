1d60628ecbd6a88f848166b5062c743b9e261943
jasminaramim <jasminaramim2005@gmail.com>
2025-06-18 22:30:23 +0600

    bugs fixed

--- Dashboard1-frontend/src/pages/Dashboard/pages/From/Tables/DataTables.jsx
+++ Dashboard1-frontend/src/pages/Dashboard/pages/From/Tables/DataTables.jsx
@@ -125,8 +125,8 @@ const DataTables = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8  sm:px-6 ">
-      <div className="max-w-4xl mx-auto space-y-8">
+    <div className="min-h-screen py-8  sm:px-6 ">
+      <div className="max-w-7xl mx-auto space-y-8">
         {/* Header */}
         <div className="text-center">
           <h1 className="text-3xl font-bold text-gray-900 mb-2">Employee Dashboard</h1>
